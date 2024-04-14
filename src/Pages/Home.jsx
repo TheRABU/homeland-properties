d7f01892d1b82c26d914412ee00503f4f229b191
TheRABU <mdfazlerabbi114@gmail.com>
2024-04-14 12:47:32 +0600

    banner created using swiper js

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,11 @@
+import Banner from "../Components/Banner/Banner";
+
 const Home = () => {
   return (
     <>
       <div>
         <h2 className="text-3xl text-center">Welcome to Homepage</h2>
+        <Banner />
       </div>
     </>
   );
